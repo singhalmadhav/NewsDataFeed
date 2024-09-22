adedbd64cf06d3797041b3675b596e7b685bdf16
Madhav Singhal <szmadhav@amazon.com>
2024-09-22 15:17:32 +0530

    Setup Dynamo Db

--- src/main/java/com/madhav/newsapi/config/DynamoDBConfig.java
+++ src/main/java/com/madhav/newsapi/config/DynamoDBConfig.java
@@ -5,13 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Configuration
 public class DynamoDBConfig {
 
     @Bean
-    public DynamoDbClient dynamoDbClient() {
+    public DynamoDbClient dynamoDbClient() throws URISyntaxException {
         return DynamoDbClient.builder()
-                .region(Region.of("us-east-1")) // Replace with your region
+                .region(Region.of("us-east-1"))
+                .endpointOverride(URI.create("http://localhost:8000"))
                 .build();
     }
 }
--- src/main/java/com/madhav/newsapi/service/impl/NewsAPIServiceImpl.java
+++ src/main/java/com/madhav/newsapi/service/impl/NewsAPIServiceImpl.java
@@ -1,7 +1,10 @@
 package com.madhav.newsapi.service.impl;
 
+import com.madhav.newsapi.model.Article;
 import com.madhav.newsapi.model.DataModel;
+import com.madhav.newsapi.repository.StoredNewsArticleRepository;
 import com.madhav.newsapi.service.NewsAPIService;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,13 +18,22 @@ public class NewsAPIServiceImpl implements NewsAPIService {
     @Autowired
     WebClient webClient;
 
+    @Autowired
+    StoredNewsArticleRepository storedNewsArticleRepositoryl;
+
+
     public Flux<DataModel> getTrendingNews() {
-        return webClient.get()
+        Flux<DataModel> dataModelFlux =  webClient.get()
                 .uri("https://newsapi.org/v2/top-headlines?country=us&apiKey=64a239894d104dc49a6c1674b2c1b2c2")
                 .retrieve()
                 .bodyToFlux(DataModel.class)
                 .doOnError(throwable -> log.error("Failed for Some Reason"));
+        DataModel dataModel = dataModelFlux.single().block();
 
+
+
+        storedNewsArticleRepositoryl.save(dataModel.getArticles().get(0));
+        return dataModelFlux;
     }
 
     public Flux<DataModel> getTrendingNewsByCategory(String category) {
